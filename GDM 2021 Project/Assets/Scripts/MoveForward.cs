597e3e24b6276899a8b94fb2184fc3f5d8e9d69c
Ghesagan <chandler.ochs1357@gmail.com>
2021-01-16 11:22:56 -0500

    Base Project Added
    Base code for top down shooter implemented. Placeholders used for player, enemy, and projectiles.

---
+++ GDM 2021 Project/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    // base speed of the movement
+    public float speed = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the object towards the player at the specifed speed
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+    }
+}
